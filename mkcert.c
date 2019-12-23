f2aac1418b45e896bb6ceaf666eff7eebf47d853
Bill-Gray <pluto@projectpluto.com>
2019-12-23 12:35:34 -0500

    Revised 'mkcert' comments;  added GPL statement

--- mkcert.c
+++ mkcert.c
@@ -1,3 +1,20 @@
+/* Copyright (C) 2018, Project Pluto
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA. */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -12,17 +29,21 @@ control) of a site by putting certain files containing random strings into
 the /.well-known/acme-challenge directory of that site.  If you have
 shell access,  that can be essentially automatic.  If (like me) you don't,
 you have to go through some tedium.  The certificates have a three-month
-lifetime,  so you have to do it just often enough to be annoying.  This
-bit of code reduces (very slightly) the tedium of the process.
+lifetime,  so you have to do it roughly every 2.5 months.  You run the
+'certbot' program;  for each certificate to be renewed,  it gives you a
+87-byte random string that must be placed in a file with the name
+/.well-known/acme-challenge/(first 43 bytes of the random string).
+Then you hit a key and repeat for the next certificate.
 
-   Each request consists of an 87-byte random string.  You're asked to put
-that into the file /.well-known/acme-challenge/(first 43 bytes of that
-random string).  This program takes the random string as a command line
-argument and makes the file in question,  then uses sftp to upload it to
-the correct spot on my ISP's server.
+   This bit of code reduces (very slightly) the tedium of the process.
+It takes the 87-byte random string as a command-line argument.  It
+then makes the required file and uploads it to the correct spot on
+my ISP's server via sftp.  (You'll presumably have to modify the
+server name and output directory,  of course.)
 
    Ideally,  I'd also automate putting the certificate and key file,
-and perhaps the installation.  At present,  I do that using cPanel. */
+and perhaps the installation,  but I'm a little unclear as to how
+to do that.  At present,  I do that using cPanel. */
 
 #define NAME_LEN 44
 
