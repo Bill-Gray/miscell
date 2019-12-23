0a3a914f22dc1fc1ec8c3e702fc8f5ba0fbe0964
Bill-Gray <pluto@projectpluto.com>
2019-12-23 12:38:47 -0500

    Added GPL statements to code missing them

--- xfer2.cpp
+++ xfer2.cpp
@@ -1,3 +1,20 @@
+/* Copyright (C) 2018, Project Pluto
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA. */
+
 /* Assume we need to solve for a transfer orbit wherein the object
 starts out at distance r1 from the sun,  and gets to a distance r2
 at an angle theta0 (as seen from the sun) at some time delta_t later.
