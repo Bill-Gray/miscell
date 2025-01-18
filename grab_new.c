77a62ffc317d371d3d21fa820dfd720c2c327a24
Bill-Gray <pluto@projectpluto.com>
2025-01-17 21:22:56 -0500

    (Forehead slap) Had a redundant /* within an already-started comment

--- grab_new.c
+++ grab_new.c
@@ -17,7 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 02110-1301, USA.
 
-/* Code to download ADES-formatted astrometry from the MPC observations
+Code to download ADES-formatted astrometry from the MPC observations
 API,  using the methods described at
 
 https://minorplanetcenter.net/mpcops/documentation/neocp-observations-api/
