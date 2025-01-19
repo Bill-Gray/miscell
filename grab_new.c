0437e6d0a16faf3ea323203ac73f09ab0ef29205
Bill-Gray <pluto@projectpluto.com>
2025-01-19 10:11:18 -0500

    'grab_new.c' can now get NEOCP data.  It does so by the admittedly stupid expedient of looking for data on a designated object,  and if that fails,  looking for NEOCP data.

--- grab_new.c
+++ grab_new.c
@@ -58,6 +58,7 @@ NEOCP as well;  we'd presumably use a shorter delay there.     */
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <assert.h>
 #include "stringex.h"
@@ -78,50 +79,19 @@ static int grab_file( const char *url, const char *outfilename)
    return( err_code);
 }
 
-int main( const int argc, const char **argv)
+static int verbose = 0;
+
+static int download_astrometry( const char *filename, const char *object_desig,
+                  const bool is_neocp)
 {
-   int err_code, verbose = 0, is_neocp = 0;
-   size_t i;
-   FILE *fp;
-   char object_desig[30], command[250];
-   const char *filename = argv[1];
+   FILE *fp = fopen( filename, "rb");
+   int err_code, rval = 0;
+   char command[250];
    const time_t t0 = time( NULL);
    const char *format = "curl -X GET -H \"Content-Type: application/json\" " \
               "-d '{ \"%s\": [\"%s\"], \"output_format\":[\"XML\"]}' "  \
               "https://data.minorplanetcenter.net/api/get-obs%s -o%s -s";
 
-   assert( argc > 2);
-   if( 3 == argc)                /* simply downloading a file */
-      if( !memcmp( argv[2], "http", 4) || !memcmp( argv[2], "ftp", 3))
-         return( grab_file( argv[2], argv[1]));
-   *object_desig = '\0';
-   for( i = 2; i < (size_t)argc; i++)
-      if( argv[i][0] != '-')    /* not a command-line option; */
-         {                      /* presumed to be part of object name */
-         if( *object_desig)
-            strlcat_error( object_desig, " ");
-         strlcat_error( object_desig, argv[i]);
-         }
-      else switch( argv[i][1])
-         {
-         case 'v':
-            verbose = 1;
-            break;
-         case 'n':
-            is_neocp = 1;
-            break;
-         case 't':
-            delay_between_reloads = atoi( argv[i] + 2);
-            break;
-         default:
-            fprintf( stderr, "Argument '%s' not recognized\n", argv[i]);
-            return( -1);
-         }
-   object_desig[sizeof( object_desig) - 1] = '\0';
-   assert( strlen( object_desig) < sizeof( object_desig) - 1);
-   if( verbose)
-      printf( "Object desig '%s'\n", object_desig);
-   fp = fopen( filename, "rb");
    if( fp)
       {
       char tbuff[90];
@@ -145,7 +115,6 @@ int main( const int argc, const char **argv)
       fclose( fp);
       }
 
-
    snprintf( command, sizeof( command), format,
                   (is_neocp ? "trksubs" : "desigs"),
                   object_desig,
@@ -155,10 +124,13 @@ int main( const int argc, const char **argv)
       printf( "%s\n", command);
    err_code = system( command);
    if( err_code)
+      {
       fprintf( stderr, "Error %d\n", err_code);
+      rval = -3;
+      }
    else
       {
-      size_t j, len, bytes_read;
+      size_t i, j, len, bytes_read;
       char *buff, *tptr;
 
       fp = fopen( filename, "rb");
@@ -181,19 +153,63 @@ int main( const int argc, const char **argv)
             buff[j] = buff[i];
       buff[j] = '\0';
       tptr = strstr( buff, "</ades>");
-      assert( tptr);
-      tptr[7] = '\n';
-      tptr[8] = '\0';
-      tptr = strstr( buff, "<ades version=");
-      assert( tptr);
-      fp = fopen( filename, "wb");
-      assert( fp);
+      if( tptr)
+         {
+         tptr[7] = '\n';
+         tptr[8] = '\0';
+         tptr = strstr( buff, "<ades version=");
+         assert( tptr);
+         fp = fopen( filename, "wb");
+         assert( fp);
 
-      fprintf( fp, "COM UNIX time %ld (%.24s)\n", (long)t0, asctime( gmtime( &t0)));
-      fprintf( fp, "COM Obj %s\n", object_desig);
-      fwrite( tptr, 1, strlen( tptr), fp);
-      fclose( fp);
+         fprintf( fp, "COM UNIX time %ld (%.24s)\n", (long)t0, asctime( gmtime( &t0)));
+         fprintf( fp, "COM Obj %s\n", object_desig);
+         fwrite( tptr, 1, strlen( tptr), fp);
+         fclose( fp);
+         }
+      else
+         rval = -1;
       free( buff);
       }
-   return( 0);
+   return( rval);
+}
+
+int main( const int argc, const char **argv)
+{
+   size_t i;
+   char object_desig[30];
+   int rval;
+
+   assert( argc > 2);
+   if( 3 == argc)                /* simply downloading a file */
+      if( !memcmp( argv[2], "http", 4) || !memcmp( argv[2], "ftp", 3))
+         return( grab_file( argv[2], argv[1]));
+   *object_desig = '\0';
+   for( i = 2; i < (size_t)argc; i++)
+      if( argv[i][0] != '-')    /* not a command-line option; */
+         {                      /* presumed to be part of object name */
+         if( *object_desig)
+            strlcat_error( object_desig, " ");
+         strlcat_error( object_desig, argv[i]);
+         }
+      else switch( argv[i][1])
+         {
+         case 'v':
+            verbose = 1;
+            break;
+         case 't':
+            delay_between_reloads = atoi( argv[i] + 2);
+            break;
+         default:
+            fprintf( stderr, "Argument '%s' not recognized\n", argv[i]);
+            return( -1);
+         }
+   object_desig[sizeof( object_desig) - 1] = '\0';
+   assert( strlen( object_desig) < sizeof( object_desig) - 1);
+   if( verbose)
+      printf( "Object desig '%s'\n", object_desig);
+   rval = download_astrometry( argv[1], object_desig, false);
+   if( -1 == rval && strlen( object_desig) < 8)
+      rval = download_astrometry( argv[1], object_desig, true);
+   return( rval);
 }
