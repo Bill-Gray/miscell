494e42d508c269adc11622d94244f60f29c498d9
Bill-Gray <pluto@projectpluto.com>
2026-05-13 11:46:57 -0400

    'radar.c' : DSS-63 (Madrid) and DSS-70 (Lovell 76-m) now have official MPC codes,  (R47) and (R46) respectively.  So my unofficial codes can be replaced with official ones.

--- radar.c
+++ radar.c
@@ -110,10 +110,10 @@ static void put_mpc_code_from_dss( char *mpc_code, const int dss_desig)
          code = "271";     /* DSS-47 in Australia */
          break;
       case -63:
-         code = "d63";     /* DSS-63 (70-m),  Madrid */
+         code = "R47";     /* DSS-63 (70-m),  Madrid */
          break;
       case -70:
-         code = "d70";     /* Lovell (76-m, Jodrell Bank) */
+         code = "R46";     /* Lovell (76-m, Jodrell Bank) */
          break;
       case -73:
          code = "273";
